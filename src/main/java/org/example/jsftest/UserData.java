832420b0a229d2b172484432c08a46059f4a721b
Alin Bordeianu <martynnn33@gmail.com>
2025-10-22 09:16:30 +0200

    feat: used custom component

---
+++ src/main/java/org/example/jsftest/UserData.java
@@ -0,0 +1,35 @@
+package org.example.jsftest;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Named("userData")
+@SessionScoped
+public class UserData implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        return "loginResult";
+    }
+}
